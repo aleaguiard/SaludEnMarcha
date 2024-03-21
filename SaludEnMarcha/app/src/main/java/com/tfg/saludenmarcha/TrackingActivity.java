693b98ec19017bb3412d677e3d3ab5569b5fec4b
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-03-21 17:49:07 +0100

    mapas googlemaps en actividad funcionando

--- SaludEnMarcha/app/src/main/java/com/tfg/saludenmarcha/TrackingActivity.java
+++ SaludEnMarcha/app/src/main/java/com/tfg/saludenmarcha/TrackingActivity.java
@@ -8,11 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class TrackingActivity extends AppCompatActivity {
+public class TrackingActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -20,16 +23,16 @@ public class TrackingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tracking);
+        setContentView(R.layout.activity_tracking); // Mover esta línea aquí
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync((OnMapReadyCallback) this);
-
     }
 
     public void onMapReady(GoogleMap googleMap) {
@@ -43,5 +46,4 @@ public class TrackingActivity extends AppCompatActivity {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ciudadReal));
     }
 
-    //que pueda escuchar el clic
 }
