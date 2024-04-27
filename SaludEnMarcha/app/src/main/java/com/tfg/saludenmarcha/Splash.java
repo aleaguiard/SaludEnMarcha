f81c345dac047b7f37e261bcdbb254aea5aebc3b
Ale Aguiar Díaz <122371223+aleaguiard@users.noreply.github.com>
2024-04-27 16:55:30 +0100

    Creación splash

---
+++ SaludEnMarcha/app/src/main/java/com/tfg/saludenmarcha/Splash.java
@@ -0,0 +1,35 @@
+package com.tfg.saludenmarcha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this , Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
